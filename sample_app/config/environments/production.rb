299f81b3035ee82a04912322b7079f000ebc00a5
llp0574 <563179705@qq.com>
2017-06-14 17:58:48 +0800

    Fix

--- sample_app/config/environments/production.rb
+++ sample_app/config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
-  onfig.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'rails-full-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
